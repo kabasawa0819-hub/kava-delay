4f5a449e5b15e5df2a8c07d23246aaa6f93dd6a2
kabasawa0819-hub <kabasawa0819@gmail.com>
2026-08-03 17:32:28 +0900

    Bump offline cache for stability fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kava-delay-v6-racefix';
+const CACHE = 'kava-delay-v7-stable';
 const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
