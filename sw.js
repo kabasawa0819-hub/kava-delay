93f1529390cfb65c7c0f3e9721b9985439af95f5
kabasawa0819-hub <kabasawa0819@gmail.com>
2026-08-04 09:09:27 +0900

    Refresh cache for higher quality video build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kava-delay-v10-countdown-fix';
-const ASSETS = ['./','./index.html','./style.css','./app.js','./countdown-fix.js','./manifest.webmanifest'];
+const CACHE = 'kava-delay-v11-hq';
+const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
